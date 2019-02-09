cc5576b38c0cea312b1e202cd53517c9b2e3e174
Joshua Schmidt <schmidt.uconn@gmail.com>
2019-02-08 21:33:08 -0500

    Fixed bug where if the file was deleted all files after it are deleted from storage

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,11 @@
 const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+const {Storage} = require('@google-cloud/storage');
+
+const storage = new Storage();
+const bucketName = 'markd-schmidt-happens.appspot.com';
+
 admin.initializeApp();
 
 //Used to delete old image when new image is loaded for home
@@ -53,35 +58,28 @@ exports.logoImageUpdated = functions.database.ref('/users/{contractorId}/logoFil
             });
 });
 
-exports.plumbingServiceImageUpdated = functions.database.ref('/users/{customerId}/plumbingServices/{serviceId}/files/{fileId}')
+exports.plumbingServiceImageUpdated = functions.database.ref('/users/{customerId}/plumbingServices/{serviceId}/files')
     .onWrite((change, context) => {
         const customerId = context.params.customerId
         if (!change.before.exists()) {
-            console.log("No previous service image exists.");
-            return Promise.resolve("No previous service")
+            console.log("No previous files exists.");
+            return Promise.resolve("No previous files.")
         }
-        const oldFile = change.before.val().guid;
-        if(change.after.exists()) {
-            const newFile = change.after.val().guid;
-
-            if(oldFile == newFile) {
-                console.log("No change to file")
-                return Promise.resolve("No change")
-            }
+        const oldFiles = change.before.val();
+        var newFiles = [];
+        if(!change.after.exists()) {
+            console.log("Files have been deleted.");
+        } else {
+            newFiles = change.after.val();
         }
 
-        const {Storage} = require('@google-cloud/storage');
-        const storage = new Storage();
-        const bucketName = 'markd-schmidt-happens.appspot.com';
-        const filename = 'images/services/' + customerId + "/" + oldFile;
-
-        // Deletes the file from the bucket
-        return storage.bucket(bucketName).file(filename).delete()
-            .then(() => {
-              console.log(`gs://${bucketName}/${filename} deleted.`);
+        return Promise.all(oldFiles.filter((oldFile) => oldFile && oldFile.guid).map((oldFile) =>
+            deleteOldFile(customerId, oldFile, newFiles)))
+            .then(_ => {
+                console.log("Successfully processed files.")
             })
-            .catch(err => {
-              console.error('ERROR:', err);
+            .catch(error => {
+                console.error(error)
             });
 });
 
@@ -89,31 +87,24 @@ exports.electricalServiceImageUpdated = functions.database.ref('/users/{customer
     .onWrite((change, context) => {
         const customerId = context.params.customerId
         if (!change.before.exists()) {
-            console.log("No previous service image exists.");
-            return Promise.resolve("No previous service image")
+            console.log("No previous files exists.");
+            return Promise.resolve("No previous files.")
         }
-        const oldFile = change.before.val().guid;
-        if(change.after.exists()) {
-            const newFile = change.after.val().guid;
-
-            if(oldFile == newFile) {
-                console.log("No change to file")
-                return Promise.resolve("No change.")
-            }
+        const oldFiles = change.before.val();
+        var newFiles = [];
+        if(!change.after.exists()) {
+            console.log("Files have been deleted.");
+        } else {
+            newFiles = change.after.val();
         }
 
-        const {Storage} = require('@google-cloud/storage');
-        const storage = new Storage();
-        const bucketName = 'markd-schmidt-happens.appspot.com';
-        const filename = 'images/services/' + customerId + "/" + oldFile;
-
-        // Deletes the file from the bucket
-        return storage.bucket(bucketName).file(filename).delete()
-            .then(() => {
-              console.log(`gs://${bucketName}/${filename} deleted.`);
+        return Promise.all(oldFiles.filter((oldFile) => oldFile && oldFile.guid).map((oldFile) =>
+            deleteOldFile(customerId, oldFile, newFiles)))
+            .then(_ => {
+                console.log("Successfully processed files.")
             })
-            .catch(err => {
-              console.error('ERROR:', err);
+            .catch(error => {
+                console.error(error)
             });
 });
 
@@ -121,37 +112,42 @@ exports.hvacServiceImageUpdated = functions.database.ref('/users/{customerId}/hv
     .onWrite((change, context) => {
         const customerId = context.params.customerId
         if (!change.before.exists()) {
-            console.log("No previous service image exists.");
-            return Promise.resolve("No previous service image")
+            console.log("No previous files exists.");
+            return Promise.resolve("No previous files.")
         }
-        const oldFile = change.before.val().guid;
-        if(!oldFile) {
-            console.log("No previous service image exists.");
-            return Promise.resolve("No previous service image")
-        }
-        if(change.after.exists()) {
-            const newFile = change.after.val().guid;
-
-            if(oldFile == newFile) {
-                console.log("No change to file")
-                return Promise.resolve("No change.")
-            }
+        const oldFiles = change.before.val();
+        var newFiles = [];
+        if(!change.after.exists()) {
+            console.log("Files have been deleted.");
+        } else {
+            newFiles = change.after.val();
         }
 
-        const {Storage} = require('@google-cloud/storage');
-        const storage = new Storage();
-        const bucketName = 'markd-schmidt-happens.appspot.com';
-        const filename = 'images/services/' + customerId + "/" + oldFile;
+        return Promise.all(oldFiles.filter((oldFile) => oldFile && oldFile.guid).map((oldFile) =>
+            deleteOldFile(customerId, oldFile, newFiles)))
+            .then(_ => {
+                console.log("Successfully processed files.")
+            })
+            .catch(error => {
+                console.error(error)
+            });
+});
 
+function deleteOldFile(customerId, oldFile, newFiles) {
+    const oldGuid = oldFile.guid;
+    if(newFiles.some(newFile => newFile.guid === oldGuid)) {
+        //do nothing
+        console.log('No need to delete ' + oldGuid);
+        return Promise.resolve()
+    } else {
+        const filename = 'images/services/' + customerId + "/" + oldGuid;
         // Deletes the file from the bucket
         return storage.bucket(bucketName).file(filename).delete()
             .then(() => {
-              console.log(`gs://${bucketName}/${filename} deleted.`);
-            })
-            .catch(err => {
-              console.error('ERROR:', err);
+                console.log(`gs://${bucketName}/${filename} deleted.`);
             });
-});
+    }
+}
 
 //Used to send Push Notifications when new notification exists
 exports.notifications = functions.database.ref('/notifications/{customerId}')
